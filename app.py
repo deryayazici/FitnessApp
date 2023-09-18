e1c20fbdf32639cad1eca830380bce3fa49b3bfa
DERYA YAZICI <deryayazici14@gmail.com>
2023-09-18 14:01:55 -0400

    first commit

---
+++ app.py
@@ -0,0 +1,19 @@
+import sys
+from flask import Flask, render_template
+
+print(sys.executable)
+
+
+app = Flask(__name__)
+# Set longer cache times to improve performance. Short cache times are primarily for development and debugging purposes.
+@app.after_request
+def add_cache_control(response):
+    response.headers['Cache-Control'] = 'public, max-age=1'
+    return response
+
+@app.route("/")
+def home():
+        return render_template ('home.html')
+
+if __name__ == '__main__':
+    app.run(debug=True)
