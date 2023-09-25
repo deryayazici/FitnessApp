34facf2f36872fb217714103028904d06ca0de3b
DERYA YAZICI <deryayazici14@gmail.com>
2023-09-25 14:21:27 -0400

    post method added to app.py

--- app.py
+++ app.py
@@ -1,23 +1,27 @@
 import sys
-from flask import Flask, render_template
+from flask import Flask, render_template, request
+import pdb
 
 print(sys.executable)
 
 
 app = Flask(__name__)
-# Set longer cache times to improve performance. Short cache times are primarily for development and debugging purposes.
-@app.after_request
-def add_cache_control(response):
-    response.headers['Cache-Control'] = 'public, max-age=1'
-    return response
+
 
 @app.route("/")
 def home():
         return render_template ('home.html')
 
-@app.route("/running")
+@app.route("/running", methods=["GET","POST"])
 def running():
-     return render_template("running.html")
+     pdb.set_trace()
+     return render_template('running.html')
+
+# Set longer cache times to improve performance. Short cache times are primarily for development and debugging purposes.
+@app.after_request
+def add_cache_control(response):
+    response.headers['Cache-Control'] = 'public, max-age=1'
+    return response
 
 if __name__ == '__main__':
     app.run(debug=True)
