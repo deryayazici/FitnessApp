d757d2914ca5756aa636c14f536715ed8d6874d6
DERYA YAZICI <deryayazici14@gmail.com>
2023-10-20 14:18:53 -0400

    db_init.py added

--- app.py
+++ app.py
@@ -1,9 +1,6 @@
 import sys
 from flask import Flask, render_template, request, redirect, url_for, g 
 import sqlite3
-import pdb
-
-print(sys.executable)
 
 
 app = Flask(__name__)
@@ -13,47 +10,50 @@ app = Flask(__name__)
 def home():
         return render_template ('home.html')
 
+
 @app.route("/running", methods=["GET","POST"])
 def running():
-     pdb.set_trace()
      return render_template('running.html')
+
 
 @app.route("/exercise", methods=["GET", "POST"])
 def exercise():
-    conn = get_db()
-    c = conn.cursor()
     if request.method == "POST":
-         
-         c.execute("""INSERT INTO fitness(id,title, description)
+        conn = get_db()
+        c = conn.cursor()
+        c.execute("""INSERT INTO fitness(id,title, description)
                     VALUES(?,?,?)""",
                     (
                         request.form.get("title"),
                         request.form.get("description")
                     )
         )
-    conn.commit()
+        conn.commit()
+        conn.close()
         #  print("Form data:")
         #  print("Title: {}, Description: {}".format(
         #        request.form.get("title"), request.form.get("description")
         #  ))
-    return redirect(url_for("home"))
+        return redirect(url_for("home"))
     return render_template('exercise.html')
 
 def get_db():
      db = getattr(g, "_database", None)
      if db is None:
-          db = g._database = sqlite3.connect("db/fitness.db")
+          db = g._database = sqlite3.connect("db/fitness.db") 
      return db
 
-def close_connection():
+@app.teardown_appcontext
+def close_connection(exception):
      db = getattr(g, "_database", None)
      if db is not None:
-          db.close(Exception)
-
-@app.after_request
+        db.close()
+     
+@app.after_request  
 def add_cache_control(response):
     response.headers['Cache-Control'] = 'public, max-age=1'
     return response
+
 
 if __name__ == '__main__':
     app.run(debug=True)
---
+++ static/db/db_init.py
@@ -0,0 +1,27 @@
+import sqlite3
+
+db_file = 'fitness.db'
+
+def create_table():
+
+    conn = sqlite3.connect(db_file)
+    
+    cursor = conn.cursor()
+    
+    create_table_sql = '''
+    CREATE TABLE IF NOT EXISTS fitness (
+        id INTEGER PRIMARY KEY,
+        title TEXT,
+        description TEXT
+    )
+    '''
+    
+    cursor.execute(create_table_sql)
+    
+    conn.commit()
+    conn.close()
+
+if __name__ == '__main__':
+    
+    create_table()
+    print(f'Table created in {db_file}')
