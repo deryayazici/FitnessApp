f6d5ba6d8f313b57574a0a8e17fe91870acd8cf8
DERYA YAZICI <deryayazici14@gmail.com>
2023-10-31 15:13:49 -0400

    image

--- app.py
+++ app.py
@@ -55,7 +55,7 @@ def exercise():
                 image_filename = secure_filename(image_file.filename)
                 # image_path = os.path.join(app.root_path, "static/images", image_file.filename)
                 # image_path = os.path.join("Users/deryazici/fitness/static/images", image_file.filename)
-                image_path = os.path.join("uploads", image_filename)
+                image_path = os.path.join("static/uploads", image_filename)
                 image_file.save(image_path)
 
                 c.execute("""INSERT INTO fitness(id,title, description, image)
@@ -76,9 +76,9 @@ def exercise():
         return "No image file selected"
     return render_template('exercise.html')
 
-@app.route('/static/images/<filename>')
+@app.route('/static/uploads/<filename>')
 def serve_image(filename):
-    return send_from_directory('static/images', filename)
+    return send_from_directory('static/uploads', filename)
 
 @app.route("/delete_item/<int:item_id>", methods=["POST"])
 def delete_item(item_id):
