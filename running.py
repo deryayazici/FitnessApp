77486dface405eefbc67670480883ddfe04e3768
DERYA YAZICI <deryayazici14@gmail.com>
2023-12-01 14:16:20 -0500

    running modified

--- running.py
+++ running.py
@@ -2,13 +2,12 @@ from activities import PhysicalActivity
 
 class Running(PhysicalActivity):
     def __init__(self, distance, weight):
-        super().__init__(weight)
-        self.distance = distance
+        super().__init__(weight) 
+        self.distance = distance  
 
-
-class Running(PhysicalActivity):
     def calculate_calories_burned(self):
         distance_km = self.distance * 1.60934
         weight_kg = self.weight * 0.453592
         calories_burned_per_km = 0.75 * weight_kg
         return int(calories_burned_per_km * distance_km)
+
