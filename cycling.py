457fcf1f91f7538bed1b20642f6615c25eefadee
DERYA YAZICI <deryayazici14@gmail.com>
2023-12-01 15:14:29 -0500

    change

---
+++ cycling.py
@@ -0,0 +1,18 @@
+from activities import PhysicalActivity
+
+class Cycling(PhysicalActivity):
+    def __init__(self, distance, weight):
+        super().__init__(weight) 
+        self.distance = distance  
+
+    def calculate_calories_burned(self):
+        distance_km = self.distance * 1.60934
+        weight_kg = self.weight * 0.453592
+
+        average_speed_kmh = 15
+        calories_burned_per_hour_per_kg = 8.5
+        total_hours = distance_km / average_speed_kmh
+
+        total_calories_burned = calories_burned_per_hour_per_kg * weight_kg * total_hours
+
+        return int(total_calories_burned)
