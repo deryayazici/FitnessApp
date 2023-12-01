04fc344dbaafbd725fc3a4da77ccf8dbf7bc1fff
DERYA YAZICI <deryayazici14@gmail.com>
2023-12-01 14:52:50 -0500

    change on walking

---
+++ walking.py
@@ -0,0 +1,12 @@
+from activities import PhysicalActivity
+
+class Walking(PhysicalActivity):
+    def __init__(self, distance, weight):
+        super().__init__(weight) 
+        self.distance = distance  
+
+    def calculate_calories_burned(self):
+        distance_km = self.distance * 1.60934
+        weight_kg = self.weight * 0.453592
+        calories_burned_per_km = 0.5 * weight_kg
+        return int(calories_burned_per_km * distance_km)
