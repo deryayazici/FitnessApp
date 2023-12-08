c5ce62ccadadd94171cbe1b505d2b56034d89e2d
DERYA YAZICI <deryayazici14@gmail.com>
2023-12-08 17:19:15 -0500

    sign in route modified

--- sign_in.py
+++ sign_in.py
@@ -1,9 +1,15 @@
 import sqlite3
 
-conn = sqlite3.connect('user_data.db')
-cursor = conn.cursor()
+def get_db():
+    if 'db' not in g:
+        g.db = sqlite3.connect("users.db")
+    return g.db
 
-cursor.execute('''
+
+conn = get_db
+c  = conn.cursor()
+
+c.execute('''
 CREATE TABLE IF NOT EXISTS users (
     id INTEGER PRIMARY KEY,
     username TEXT NOT NULL UNIQUE,
@@ -15,19 +21,19 @@ conn.commit()
 
 def create_user(username, password,email):
 
-    cursor.execute("SELECT username FROM users WHERE username = ?", (username,))
-    if cursor.fetchone():
+    c.execute("SELECT username FROM users WHERE username = ?", (username,))
+    if c.fetchone():
         return "Username already exists. Please choose another one."
 
     try:
         
-        cursor.execute("INSERT INTO users (username, password, email) VALUES (?, ?,?)", (username, password,email))
+        c.execute("INSERT INTO users (username, password, email) VALUES (?, ?,?)", (username, password,email))
         conn.commit()
         return "User created successfully"
     except sqlite3.IntegrityError:
         return "There was an error creating the user."
 
-def login_user(username, password):
-    cursor.execute("SELECT * FROM users WHERE username=? AND password=?", (username, password))
-    user = cursor.fetchone()
-    return "Login successful" if user else "Invalid username or password"
+# def login_user(username, password):
+#     cursor.execute("SELECT * FROM users WHERE username=? AND password=?", (username, password))
+#     user = cursor.fetchone()
+#     return "Login successful" if user else "Invalid username or password"
